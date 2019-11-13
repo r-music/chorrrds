7646b4dd5b317cbaed1611c24b7749d2c690c25f
leonawicz <matt_leonawicz@esource.com>
2019-11-12 19:22:31 -0700

    Upgraded imports from XML to xml2/rvest. Simplified the xml extraction, minor bug fixes. Added unit tests for example song and chord extraction. Updated relevant documentation.

--- tests/testthat/test-chorrrds.R
+++ tests/testthat/test-chorrrds.R
@@ -1,5 +1,21 @@
 context("test-chorrrds")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+x <- get_songs("tim-maia")
+
+test_that("songs returned as expected", {
+  expect_is(x, "data.frame")
+  expect_true(all(sapply(x, is.character)))
+  expect_true(nrow(x) > 100)
+  expect_equal(names(x), c("url", "song"))
+  expect_false(any(grepl("letra", x$url)))
+})
+
+test_that("chords returned as expected", {
+  idx <- which(x$url == "/tim-maia/a-paz-do-meu-mundo-e-voce/")
+  chords <- get_chords(x$url[idx])
+  
+  expect_is(chords, "data.frame")
+  expect_true(all(sapply(chords, is.character)))
+  expect_equal(dim(chords), c(54, 4))
+  expect_equal(names(chords), c("chords", "key", "song", "artist"))
 })
